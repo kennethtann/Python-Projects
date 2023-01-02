f5481b6febb9ff45fb1a5f4acb4b7c25ededa854
kenneth <kennethlovecats@gmail.com>
2023-01-02 16:26:36 +0800

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,68 @@
+from replit import clear
+
+logo = """
+           _            _       _             
+          | |          | |     | |            
+  ___ __ _| | ___ _   _| | __ _| |_ ___  _ __ 
+ / __/ _` | |/ __| | | | |/ _` | __/ _ \| '__|
+| (_| (_| | | (__| |_| | | (_| | || (_) | |   
+ \___\__,_|_|\___|\__,_|_|\__,_|\__\___/|_|   
+ 
+ _____________________
+|  _________________  |
+| | JO           0. | |
+| |_________________| |
+|  ___ ___ ___   ___  |
+| | 7 | 8 | 9 | | + | |
+| |___|___|___| |___| |
+| | 4 | 5 | 6 | | - | |
+| |___|___|___| |___| |
+| | 1 | 2 | 3 | | x | |
+| |___|___|___| |___| |
+| | . | 0 | = | | / | |
+| |___|___|___| |___| |
+|_____________________|
+"""
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+def calculator():
+  print(logo)
+
+  num1 = float(input("What's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+ 
+  while should_continue:
+    operation_symbol = input("Pick an operation: ")
+    num2 = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+      num1 = answer
+    else:
+      should_continue = False
+      clear()
+      calculator()
+
+calculator()
