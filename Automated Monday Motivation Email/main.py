45f6f649f3144c0f900b7836ade1c0ba49144759
kenneth <kennethlovecats@gmail.com>
2023-02-14 23:55:56 +0800

    Add files via upload

---
+++ Automated Monday Motivation Email/main.py
@@ -0,0 +1,24 @@
+import smtplib
+import datetime as dt
+import random
+
+my_email = "automatedmotionalert@gmail.com"
+password = "mxgjqzsmjcqfzjnx"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 0:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = random.choice(all_quotes)
+
+    print(quote)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(my_email, password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=my_email,
+            msg=f"Subject:Monday Motivation\n\n{quote}"
+        )
+
